1884fc3f83a220f8a7c1c14a29f9df70a2cddd5f
s2267810 <t.meulenkamp@student.utwente.nl>
2021-12-05 20:54:47 +0100

    Day 5 - Code Rewrite

--- days/day5.py
+++ days/day5.py
@@ -1,5 +1,26 @@
 from days.day import Day
-import numpy as np
+
+
+def fill_field(field, coordinates, use_diagonal):
+    for (p1, p2, is_vertical, is_horizontal, is_x_positive, is_y_positive) in coordinates:
+        x = 1
+        y = 1
+        if not is_x_positive:
+            x = -1
+        if not is_y_positive:
+            y = -1
+
+        if is_horizontal:
+            for i in range(abs(p1[0] - p2[0]) + 1):
+                field[p1[1]][p1[0] + (x * i)] += 1
+        elif is_vertical:
+            for i in range(abs(p1[1] - p2[1]) + 1):
+                field[p1[1] + (y * i)][p1[0]] += 1
+        elif use_diagonal:
+            for i in range(abs(p1[0] - p2[0]) + 1):
+                field[p1[1] + (y * i)][p1[0] + (x * i)] += 1
+
+    return field
 
 
 class Day5(Day):
@@ -8,64 +29,26 @@ class Day5(Day):
         super().__init__(5, "Hydrothermal Venture", input_type="str")
         self.coordinates = [[[int(x) for x in p.split(",")] for p in line.split(" -> ")] for line in self.input]
 
-    def part_a(self):
-
-        # (p1, p2, is_vertical, div(p1,p2) > 0)
-        used_coords = []
+    def add_meta_data(self):
+        coordinates = []
         for [[x1, y1], [x2, y2]] in self.coordinates:
-            if x1 == x2:
-                used_coords.append(([x1, y1], [x2, y2], False, y1 < y2))
-            elif y1 == y2:
-                used_coords.append(([x1, y1], [x2, y2], True, x1 < x2))
+            coordinates.append(([x1, y1], [x2, y2], x1 == x2, y1 == y2, x1 < x2, y1 < y2))
+        return coordinates
 
-        max_x = max(max(used_coords, key=lambda p: p[0][0])[0][0], max(used_coords, key=lambda p: p[1][0])[1][0])
-        max_y = max(max(used_coords, key=lambda p: p[0][1])[0][1], max(used_coords, key=lambda p: p[1][1])[1][0])
+    def generate_field(self):
+        max_x = max(max(self.coordinates, key=lambda p: p[0][0])[0][0],
+                    max(self.coordinates, key=lambda p: p[1][0])[1][0])
 
-        field = [[0 for _ in range(max_x + 1)] for _ in range(max_y + 1)]
+        max_y = max(max(self.coordinates, key=lambda p: p[0][1])[0][1],
+                    max(self.coordinates, key=lambda p: p[1][1])[1][0])
+        return [[0 for _ in range(max_x + 1)] for _ in range(max_y + 1)]
 
-        for (p1, p2, is_vertical, is_positive) in used_coords:
-            j = 1
-            if not is_positive:
-                j = -1
-
-            if is_vertical:
-                for i in range(abs(p1[0] - p2[0]) + 1):
-                    field[p1[1]][p1[0] + (j * i)] += 1
-            else:
-                for i in range(abs(p1[1] - p2[1]) + 1):
-                    field[p1[1] + (j * i)][p1[0]] += 1
-
+    def part_a(self):
+        field = fill_field(self.generate_field(), self.add_meta_data(), False)
         return sum(sum(1 for c in row if c > 1) for row in field)
 
     def part_b(self):
-        # (p1, p2, is_vertical, div(p1,p2) > 0)
-        used_coords = []
-        for [[x1, y1], [x2, y2]] in self.coordinates:
-            used_coords.append(([x1, y1], [x2, y2], x1 == x2, y1 == y2, x1 < x2, y1 < y2))
-
-        max_x = max(max(used_coords, key=lambda p: p[0][0])[0][0], max(used_coords, key=lambda p: p[1][0])[1][0])
-        max_y = max(max(used_coords, key=lambda p: p[0][1])[0][1], max(used_coords, key=lambda p: p[1][1])[1][0])
-
-        field = [[0 for _ in range(max_x + 1)] for _ in range(max_y + 1)]
-
-        for (p1, p2, is_horizontal, is_vertical, is_x_positive, is_y_positive) in used_coords:
-            x = 1
-            y = 1
-            if not is_x_positive:
-                x = -1
-            if not is_y_positive:
-                y = -1
-
-            if is_vertical:
-                for i in range(abs(p1[0] - p2[0]) + 1):
-                    field[p1[1]][p1[0] + (x * i)] += 1
-            elif is_horizontal:
-                for i in range(abs(p1[1] - p2[1]) + 1):
-                    field[p1[1] + (y * i)][p1[0]] += 1
-            else:
-                for i in range(abs(p1[0] - p2[0]) + 1):
-                    field[p1[1] + (y * i)][p1[0] + (x * i)] += 1
-
+        field = fill_field(self.generate_field(), self.add_meta_data(), True)
         return sum(sum(1 for c in row if c > 1) for row in field)
 
 
