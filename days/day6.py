2e5775f3631a7bd96a13fe5fbdf1239230225ecb
s2267810 <t.meulenkamp@student.utwente.nl>
2021-12-06 09:59:27 +0100

    Day 6

---
+++ days/day6.py
@@ -0,0 +1,40 @@
+from days.day import Day
+
+
+class Day6(Day):
+
+    def __init__(self):
+        super().__init__(6, "Lantern fish")
+        self.input = [int(v) for v in self.input[0].split(",")]
+
+    def calc_fish(self, nr_days: int):
+        # Prepare dictionary
+        fish = {}
+        for i in range(9):
+            fish[i] = 0
+
+        # Fill dictionary with initial fish
+        for f in self.input:
+            fish[f] += 1
+
+        # For each day add new fish and age each existing fish
+        for _ in range(nr_days):
+            new = fish[0]
+            for i in range(8):
+                fish[i] = fish[i + 1]
+                if i == 6:
+                    fish[i] = fish[i + 1] + new
+            fish[8] = new
+
+        # Return the total number of fish
+        return sum(fish.values())
+
+    def part_a(self):
+        return self.calc_fish(80)
+
+    def part_b(self):
+        return self.calc_fish(256)
+
+
+if __name__ == '__main__':
+    Day6().run()
