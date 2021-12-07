79c8ee631e296c7af54cb8a16240bf9ed39ffb40
s2267810 <t.meulenkamp@student.utwente.nl>
2021-12-07 12:04:31 +0100

    Day 7 - One liners

--- days/day7.py
+++ days/day7.py
@@ -8,21 +8,10 @@ class Day7(Day):
         self.input = [int(x) for x in self.input[0].split(",")]
 
     def part_a(self):
-        min_fuel = max(self.input) * len(self.input)
-        for pos in range(max(self.input)):
-            fuel = sum(abs(x - pos) for x in self.input)
-            if fuel < min_fuel:
-                min_fuel = fuel
-        return min_fuel
+        return min(sum(abs(x - pos) for x in self.input) for pos in range(max(self.input)))
 
     def part_b(self):
-        # Use that fuel_use = 1/2 * (n^2 + n)
-        min_fuel = (1/2) * ((max(self.input) * len(self.input)) ** 2 + (max(self.input) * len(self.input)))
-        for pos in range(max(self.input)):
-            fuel = sum(1/2 * (abs(x - pos) ** 2 + abs(x-pos)) for x in self.input)
-            if fuel < min_fuel:
-                min_fuel = fuel
-        return min_fuel
+        return min(sum(1/2 * (abs(x - pos) ** 2 + abs(x-pos)) for x in self.input) for pos in range(max(self.input)))
 
 
 if __name__ == '__main__':
