562d7749fe8edde1d045f982158bab7f15f8a38c
Tom Meulenkamp <meulenkampt@gmail.com>
2022-11-28 00:37:34 +0100

    Run days in correct order in main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pkgutil
 
 from day_base import Day
 
-YEAR = 2021
+YEAR = 2020
 
 if __name__ == '__main__':
     # Import all the days in the days package
@@ -11,9 +11,11 @@ if __name__ == '__main__':
         importlib.import_module('.' + name, str(YEAR))
 
     # Run all the run methods of the days
-    for c in Day.__subclasses__():
+    print("Initializing day classes and fetching input data from AoC site...")
+    days = [c() for c in Day.__subclasses__()]
+    for day in sorted(days, key=lambda day: day.day_nr):
         try:
-            c().run()
+            day.run()
         except Exception as e:
-            print(f"Warning exception occurred for {c.__name__}: {e}")
+            print(f"Warning exception occurred for {day.__name__}: {e}")
             print()
