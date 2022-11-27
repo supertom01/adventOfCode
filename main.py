8a24a85a7bbc785247408086b166555c93af84e5
Tom Meulenkamp <meulenkampt@gmail.com>
2022-11-28 00:45:25 +0100

    Run days in correct order in main.py and preload all input data

--- main.py
+++ main.py
@@ -12,7 +12,14 @@ if __name__ == '__main__':
 
     # Run all the run methods of the days
     print("Initializing day classes and fetching input data from AoC site...")
-    days = [c() for c in Day.__subclasses__()]
+    percentage = 0
+    step_size = 100.0 / len(Day.__subclasses__())
+    days = []
+    for c in Day.__subclasses__():
+        days.append(c())
+        percentage += step_size
+        print(f"\r{percentage:3.0f}% done", end="")
+    print("\rFinished!\r\n")
     for day in sorted(days, key=lambda day: day.day_nr):
         try:
             day.run()
