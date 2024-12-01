93ed5025ca72efb75ee771bc47e1d1bb49cebf04
Tom Meulenkamp <meulenkampt@gmail.com>
2024-12-01 12:33:14 +0100

    2024 - Day 1: Historian Hysteria

---
+++ 2024/day1.py
@@ -0,0 +1,30 @@
+from day_base import Day
+
+
+class Day1(Day):
+    
+    def __init__(self):
+        super().__init__(2024, 1, 'Historian Hysteria', expected_a=11, expected_b=31, debug=True)
+
+    def part_a(self) -> int:
+        left = []
+        right = []
+        for line in self.input:
+            (l, r) = line.split("   ")
+            left.append(int(l))
+            right.append(int(r))
+
+        return sum(abs(l - r) for (l, r) in zip(sorted(left), sorted(right)))
+
+    def part_b(self) -> int:
+        left = []
+        right = []
+        for line in self.input:
+            (l, r) = line.split("   ")
+            left.append(int(l))
+            right.append(int(r))
+
+        return sum(right.count(l) * l for l in left)
+
+if __name__ == '__main__':
+    (Day1()).run()
